e9f0cda9649c41d1205647668753f6cf47018f38
Hiroaki0117 <nishimura.hn.0117@gmail.com>
2022-11-25 16:33:28 +0900

    修正

--- app/models/event.rb
+++ app/models/event.rb
@@ -13,7 +13,7 @@ class Event < ApplicationRecord
         return unless start_at && end_at
 
         if start_at >= end_at
-            error.add("開始時間と入力時間に不備があります")
+            errors.add(:start_at, "は終了時間よりも前に設定してください")
         end
     end
 end
